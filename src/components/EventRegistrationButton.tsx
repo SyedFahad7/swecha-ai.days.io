6a771a3e80fb9b46ecf9bd34a10afc6f041e8ba8
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2025-01-20 18:08:37 +0530

    fix express interest form height

--- src/components/EventRegistrationButton.tsx
+++ src/components/EventRegistrationButton.tsx
@@ -42,7 +42,7 @@ const EventRegistrationButton: React.FC<EventRegistrationButtonProps> = ({
             Express Interest
           </button>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px] h-screen md:h-auto bg-gray-900 border-gray-800 overflow-y-scroll">
+        <DialogContent className="max-w-lg mx-auto overflow-y-auto max-h-[80vh] px-4">
           <VisuallyHidden>
             <DialogTitle>Participant Interest Form</DialogTitle>
           </VisuallyHidden>
